66b4d3479a85124baecb2766c3603e26b095f808
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2024-01-04 21:34:14 +0530

    9. Building a react project | bgChanger modification done

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -25,11 +25,67 @@ function App() {
               Green
             </button>
 
-            <button onClick={()=> {setColor("Blue")}}
+            <button onClick={()=> {setColor("blue")}}
               className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
               style={{ backgroundColor: "blue" }}
             >
               Blue
+            </button>
+
+            <button onClick={()=> {setColor("olive")}}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "olive" }}
+            >
+              Olive
+            </button>
+
+            <button onClick={()=> {setColor("Gray")}}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "gray" }}
+            >
+              Gray
+            </button>
+
+            <button onClick={()=> {setColor("yellow")}}
+              className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+              style={{ backgroundColor: "yellow" }}
+            >
+              Yellow
+            </button>
+
+            <button onClick={()=> {setColor("pink")}}
+              className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+              style={{ backgroundColor: "pink" }}
+            >
+              Pink
+            </button>
+
+            <button onClick={()=> {setColor("purple")}}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "purple" }}
+            >
+              Purple
+            </button>
+
+            <button onClick={()=> {setColor("lavender")}}
+              className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+              style={{ backgroundColor: "lavender" }}
+            >
+              Lavender
+            </button>
+
+            <button onClick={()=> {setColor("white")}}
+              className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+              style={{ backgroundColor: "white" }}
+            >
+              White
+            </button>
+
+            <button onClick={()=> {setColor("black")}}
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "black" }}
+            >
+              Black
             </button>
           </div>
         </div>
